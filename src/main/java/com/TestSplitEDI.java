028cfddf209d5ab4f92c019c9ddd6ec12413b31a
yeeder0520 <59109960+yeeder0520@users.noreply.github.com>
2022-06-14 00:38:28 +0800

    調整為外部帶入參數

--- src/main/java/com/TestSplitEDI.java
+++ src/main/java/com/TestSplitEDI.java
@@ -1,65 +1,68 @@
 package com;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 自動產生SplitEDI.sct
+ *
  * @author YeeDer
  * @version 1.0.0
  * @since 2022/6/10 下午 06:02
  **/
 public class TestSplitEDI {
 
+//        scriptNameLists.add("splitEDI_N5102_1.sct");
+//        scriptNameLists.add("splitEDI_N5102_3.sct");
+//        scriptNameLists.add("splitEDI_N5108.sct");
+//        scriptNameLists.add("splitEDI_N5110.sct");
+//        scriptNameLists.add("splitEDI_N5110_1.sct");
+//        scriptNameLists.add("splitEDI_N5110_2.sct");
+//        scriptNameLists.add("splitEDI_N5110_3.sct");
+//        scriptNameLists.add("splitEDI_N5116.sct");
+//        scriptNameLists.add("splitEDI_N5116_1.sct");
+//        scriptNameLists.add("splitEDI_N5116_2.sct");
+//        scriptNameLists.add("splitEDI_N5116_3.sct");
+//        scriptNameLists.add("splitEDI_N5116_4.sct");
+//        scriptNameLists.add("splitEDI_N5116_5.sct");
+//        scriptNameLists.add("splitEDI_N5117.sct");
+//        scriptNameLists.add("splitEDI_N5117_1.sct");
+//        scriptNameLists.add("splitEDI_N5117_2.sct");
+//        scriptNameLists.add("splitEDI_N5117_3.sct");
+//        scriptNameLists.add("splitEDI_N5135.sct");
+//        scriptNameLists.add("splitEDI_N5135_1.sct");
+//        scriptNameLists.add("splitEDI_N5135_2.sct");
+//        scriptNameLists.add("splitEDI_N5135_3.sct");
+//        scriptNameLists.add("splitEDI_N5135_4.sct");
+//        scriptNameLists.add("splitEDI_N5135_5.sct");
+//        scriptNameLists.add("splitEDI_NX5105.sct");
+
 
     public static void main(String[] args) throws IOException {
 
-        List<String> msgTypeList = new ArrayList<>();
-        msgTypeList.add("splitEDI_N5102_1.sct");
-        msgTypeList.add("splitEDI_N5102_3.sct");
-        msgTypeList.add("splitEDI_N5108.sct");
-        msgTypeList.add("splitEDI_N5110.sct");
-        msgTypeList.add("splitEDI_N5110_1.sct");
-        msgTypeList.add("splitEDI_N5110_2.sct");
-        msgTypeList.add("splitEDI_N5110_3.sct");
-        msgTypeList.add("splitEDI_N5116.sct");
-        msgTypeList.add("splitEDI_N5116_1.sct");
-        msgTypeList.add("splitEDI_N5116_2.sct");
-        msgTypeList.add("splitEDI_N5116_3.sct");
-        msgTypeList.add("splitEDI_N5116_4.sct");
-        msgTypeList.add("splitEDI_N5116_5.sct");
-        msgTypeList.add("splitEDI_N5117.sct");
-        msgTypeList.add("splitEDI_N5117_1.sct");
-        msgTypeList.add("splitEDI_N5117_2.sct");
-        msgTypeList.add("splitEDI_N5117_3.sct");
-        msgTypeList.add("splitEDI_N5135.sct");
-        msgTypeList.add("splitEDI_N5135_1.sct");
-        msgTypeList.add("splitEDI_N5135_2.sct");
-        msgTypeList.add("splitEDI_N5135_3.sct");
-        msgTypeList.add("splitEDI_N5135_4.sct");
-        msgTypeList.add("splitEDI_N5135_5.sct");
-        msgTypeList.add("splitEDI_NX5105.sct");
 
-        for (String sct : msgTypeList) {
-            StringBuffer sb = new StringBuffer();
+        String scriptName = args[0];
+        
+        StringBuilder sb = new StringBuilder();
 
-            //寫出檔案的位置
-            FileWriter fw = new FileWriter("C:\\Users\\6620\\Desktop\\abcdefg\\產出檔案\\"+sct);
+        /*範本檔案*/
+        BufferedReader reader = Files.newBufferedReader(Paths.get("C:\\Users\\6620\\Desktop\\abcdefg\\splitEDI_N5102_1.sct"));
 
-            //範本檔案
-            FileReader fr = new FileReader("C:\\Users\\6620\\Desktop\\abcdefg\\splitEDI_N5102_1.sct");
-            BufferedReader br = new BufferedReader(fr);
+        /*寫出檔案的位置*/
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get("C:\\Users\\6620\\Desktop\\abcdefg\\產出檔案\\" + scriptName));
 
-            //取出文件別
-            String msgType = sct.substring(sct.indexOf("_") + 1, sct.lastIndexOf(".sct"));
+        /*取出文件別*/
+        String msgType = scriptName.substring(scriptName.indexOf("_") + 1, scriptName.lastIndexOf(".sct"));
 
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
 
-            String line = null;
-            while ((line = br.readLine()) != null) {
-
-                System.out.println(line);
                 if (line.contains("PgId=")) {
-                    sb.append("PgId=").append(sct).append("\n");
+                    sb.append("PgId=").append(scriptName).append("\n");
                     continue;
                 }
 
@@ -68,18 +71,18 @@ public class TestSplitEDI {
                     continue;
                 }
 
-
                 if (line.contains("CONF_FILE=${NGSC_AP_DIR}/JAVA/conf/logging-EDISplitter_")) {
                     sb.append("CONF_FILE=${NGSC_AP_DIR}/JAVA/conf/logging-EDISplitter_" + msgType + ".xml").append("\n");
                     continue;
                 }
-
-                sb.append(line + "\n");
+                sb.append(line).append("\n");
             }
-            fr.close();
-            fw.write(String.valueOf(sb));
-            fw.flush();
-            fw.close();
+
+        } finally {
+            reader.close();
+            writer.write(String.valueOf(sb));
+            writer.flush();
+            writer.close();
         }
 
 
