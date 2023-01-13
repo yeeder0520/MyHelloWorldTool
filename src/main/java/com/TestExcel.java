115bb4aacb7bbfdcb4d4bb5d4f7c549287e55b53
yeeder0520 <59109960+yeeder0520@users.noreply.github.com>
2023-01-13 16:00:04 +0800

    111

---
+++ src/main/java/com/TestExcel.java
@@ -0,0 +1,32 @@
+package com;
+
+import org.apache.poi.poifs.crypt.Decryptor;
+import org.apache.poi.poifs.crypt.EncryptionInfo;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * @author YeeDer
+ * @version 1.0.0
+ * @since 2023/1/13 下午 03:46
+ **/
+public class TestExcel {
+    public static void main(String[] args) throws Exception {
+        String src = "password_protected.xlsx";
+        String dest = "decrypted.xlsx";
+        String password = "password";
+
+        FileInputStream fis = new FileInputStream(src);
+        POIFSFileSystem fs = new POIFSFileSystem(fis);
+        EncryptionInfo info = new EncryptionInfo(fs);
+        Decryptor d = Decryptor.getInstance(info);
+        d.verifyPassword(password);
+
+        FileOutputStream fos = new FileOutputStream(dest);
+        d.writeData(fos);
+        fos.close();
+        fis.close();
+    }
+}
